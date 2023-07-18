07c04da21d110dcd06ee9c38c551627164413922
Arian <arianahmadi1@gmail.com>
2023-07-17 22:49:43 -0400

    update removing NY

--- pages/myCourts.jsx
+++ pages/myCourts.jsx
@@ -1,61 +1,73 @@
 //myCourts.js
-import { getServerSession } from "next-auth"
-import Head from "next/head";
-import { useSession } from "next-auth/react";
-import { authOptions } from "./api/auth/[...nextauth]"
-import Layout from "../src/components/layout"
-import { prisma }  from "../src/prisma";
-import { useMemo, Fragment, useState } from "react";
-import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { MapIcon } from "@heroicons/react/24/outline";
-import { Menu, Transition } from '@headlessui/react'
-import DeleteModal from "../src/components/deleteModal";
-import CourtsPanel from "../src/components/courtsPanel";
+import { getServerSession } from 'next-auth';
+import Head from 'next/head';
+import { useSession } from 'next-auth/react';
+import { authOptions } from './api/auth/[...nextauth]';
+import Layout from '../src/components/layout';
+import { prisma } from '../src/prisma';
+import { useMemo, Fragment, useState } from 'react';
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid';
+import { MapIcon } from '@heroicons/react/24/outline';
+import { Menu, Transition } from '@headlessui/react';
+import DeleteModal from '../src/components/deleteModal';
+import CourtsPanel from '../src/components/courtsPanel';
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function myCourts({ courtData }) {
-    const [deleteOpen, setDeleteOpen] = useState(false)
-    const [panelOpen, setPanelOpen] = useState(false)
-    const [currCourt, setCurrCourt] = useState(null)
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [panelOpen, setPanelOpen] = useState(false);
+  const [currCourt, setCurrCourt] = useState(null);
 
-    const { data: session, status } = useSession()
+  const { data: session, status } = useSession();
 
-    const handleDelete = (court) => {
-      setCurrCourt(court)
-      setDeleteOpen(true)
-    }
+  const handleDelete = (court) => {
+    setCurrCourt(court);
+    setDeleteOpen(true);
+  };
 
-    const handleEdit = (court) => {
-      setCurrCourt(court)
-      setPanelOpen(true)
-    }
+  const handleEdit = (court) => {
+    setCurrCourt(court);
+    setPanelOpen(true);
+  };
 
-    return (
-      <>
-        <Head>
-          <title>My Courts</title>
-          <meta name="description" content="Suggest your own court" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/ball_up.svg" />
-        </Head>
-        <Layout main={
-            <div className="pt-20 lg:pt-6 px-6 bg-slate-100 h-full">
-                <h1 className="text-2xl">My Courts</h1>
-                <ul role="list" className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 auto-rows-max">
-
-                {courtData.map((courts) => (
-                  <li key={`${courts.id}-${courts.address}`} className="col-span-1 rounded-lg bg-white shadow">
+  return (
+    <>
+      <Head>
+        <title>My Courts</title>
+        <meta name="description" content="Suggest your own court" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/ball_up.svg" />
+      </Head>
+      <Layout
+        main={
+          <div className="pt-20 lg:pt-6 px-6 bg-slate-100 h-full">
+            <h1 className="text-2xl">My Courts</h1>
+            <ul
+              role="list"
+              className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 auto-rows-max"
+            >
+              {courtData.map((courts) => (
+                <li
+                  key={`${courts.id}-${courts.address}`}
+                  className="col-span-1 rounded-lg bg-white shadow"
+                >
                   <div className="p-4 flex flex-col gap-4 h-full">
                     <div className="flex justify-between">
                       <h1>{courts.address}</h1>
-                      <Menu as="div" className="relative inline-block text-left">
+                      <Menu
+                        as="div"
+                        className="relative inline-block text-left"
+                      >
                         <div>
                           <Menu.Button className="flex items-center rounded-full bg-gray-100 text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
                             <span className="sr-only">Open options</span>
-                            <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
+                            <EllipsisVerticalIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
                           </Menu.Button>
                         </div>
 
@@ -73,35 +85,45 @@ export default function myCourts({ courtData }) {
                               <Menu.Item>
                                 {({ active }) => (
                                   <button
-                                  className={classNames(
-                                    active ? 'bg-indigo-600 text-white' : 'text-gray-700',
-                                    'block w-full px-4 py-2 text-left text-sm flex'
-                                  )}
-                                  onClick={() => window.location.replace(`/${courts.longitude},${courts.latitude},18`)}
-                                >
-                                  <p className="my-auto">Show on map</p>
-                                  <MapIcon className="h-6 w-6 pl-2"/>
-                                </button>
-                                )}
-                              </Menu.Item>
-                              <Menu.Item>
-                                {({ active }) => (
-                                  <button
-                                  className={classNames(
-                                    active ? 'bg-indigo-100 text-gray-900' : 'text-gray-700',
-                                    'block w-full px-4 py-2 text-left text-sm'
-                                  )}
-                                  onClick={() => handleEdit(courts)}
-                                >
-                                  Edit
-                                </button>
+                                    className={classNames(
+                                      active
+                                        ? 'bg-indigo-600 text-white'
+                                        : 'text-gray-700',
+                                      'block w-full px-4 py-2 text-left text-sm flex'
+                                    )}
+                                    onClick={() =>
+                                      window.location.replace(
+                                        `/${courts.longitude},${courts.latitude},18`
+                                      )
+                                    }
+                                  >
+                                    <p className="my-auto">Show on map</p>
+                                    <MapIcon className="h-6 w-6 pl-2" />
+                                  </button>
                                 )}
                               </Menu.Item>
                               <Menu.Item>
                                 {({ active }) => (
                                   <button
                                     className={classNames(
-                                      active ? 'bg-red-500 text-white' : 'text-red-500',
+                                      active
+                                        ? 'bg-indigo-100 text-gray-900'
+                                        : 'text-gray-700',
+                                      'block w-full px-4 py-2 text-left text-sm'
+                                    )}
+                                    onClick={() => handleEdit(courts)}
+                                  >
+                                    Edit
+                                  </button>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    className={classNames(
+                                      active
+                                        ? 'bg-red-500 text-white'
+                                        : 'text-red-500',
                                       'block w-full px-4 py-2 text-left text-sm'
                                     )}
                                     onClick={() => handleDelete(courts)}
@@ -116,26 +138,34 @@ export default function myCourts({ courtData }) {
                       </Menu>
                     </div>
                     <p className="text-sm">{courts.description}</p>
-
-                    
                   </div>
-                  
-                  </li>
-                ))}
-                </ul>
-                {currCourt && 
-                  <div>
-                    <DeleteModal key={currCourt.id} open={deleteOpen} setOpen={setDeleteOpen} marker={currCourt}/>
-                    <CourtsPanel key={currCourt.id} open={panelOpen} setOpen={setPanelOpen} marker={currCourt} session={session} edit={true}/>
-                  </div>
-                }
-                
-            </div>
-        }/>
-      </>
-    )
+                </li>
+              ))}
+            </ul>
+            {currCourt && (
+              <div>
+                <DeleteModal
+                  key={currCourt.id}
+                  open={deleteOpen}
+                  setOpen={setDeleteOpen}
+                  marker={currCourt}
+                />
+                <CourtsPanel
+                  key={currCourt.id}
+                  open={panelOpen}
+                  setOpen={setPanelOpen}
+                  marker={currCourt}
+                  session={session}
+                  edit={true}
+                />
+              </div>
+            )}
+          </div>
+        }
+      />
+    </>
+  );
 }
-
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -149,21 +179,18 @@ export async function getServerSideProps(context) {
     };
   }
 
-
-  const userId = (await prisma.users.findUnique({
-    where: {
+  const userId = (
+    await prisma.users.findUnique({
+      where: {
         email: session.user.email,
-    }
-  })).id
+      },
+    })
+  ).id;
   const data = await prisma.courts.findMany({
     where: {
       userId: userId,
     },
   });
-
-  console.log(data);
-  console.log(session);
-
 
   return {
     props: {
